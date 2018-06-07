b808e90ef61c872ed19f232c7714496c21bde900
Listian Pratomo <listian.pratomo@mataharimall.com>
2018-06-07 15:02:02 +0700

    change View.propTypes to PropTypes

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ class SignaturePad extends Component {
   static propTypes = {
     onChange: PropTypes.func,
     onError: PropTypes.func,
-    style: View.propTypes.style,
+    style: PropTypes.style,
     penColor: PropTypes.string,
     dataURL: PropTypes.string,
   };
